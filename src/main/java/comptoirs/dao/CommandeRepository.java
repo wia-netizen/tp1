55d044e47e0ff8b26dd8f69b2ad6a91e4d37b81a
wia-netizen <wiame.bourjila@etud.univ-jfc.fr>
2025-01-17 17:58:22 +0100

    fin de tp

--- src/main/java/comptoirs/dao/CommandeRepository.java
+++ src/main/java/comptoirs/dao/CommandeRepository.java
@@ -1,12 +1,26 @@
 package comptoirs.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import comptoirs.entity.Commande;
 import org.springframework.data.jpa.repository.Query;
-
 import java.math.BigDecimal;
-import java.util.List;
+
+import comptoirs.entity.Commande;
 
 public interface CommandeRepository extends JpaRepository<Commande, Integer> {
 
+    /**
+     * Calcule le montant des articles commandés dans une commande,
+     * en tenant compte de la remise appliquée.
+     * @param numeroCommande le numéro de la commande à traiter
+     * @return le montant total des articles commandés
+     */
+    @Query("""
+            SELECT SUM(l.quantite * l.produit.prixUnitaire * (1 - c.remise)) 
+            FROM Commande c 
+            JOIN c.lignes l
+            WHERE c.numero = :numeroCommande
+            """)
+    BigDecimal montantArticles(Integer numeroCommande);
 }
+
+
